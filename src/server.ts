c8649a6906ac96078cc7c16f41e61bc1210fcf55
sachinjadhav <sachinjdv7@gmail.com>
2025-01-03 20:36:04 +0530

    add logger

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,13 @@
 import app from './app'
 import { Config } from './config'
+import logger from './config/logger'
 
 const startSever = () => {
     const PORT = Config.PORT
     try {
-        app.listen(PORT, () => console.log(`Express Server listening ${PORT}`))
+        app.listen(PORT, () => {
+            logger.info('Listening on port', { port: PORT })
+        })
     } catch (error) {
         console.error(error)
         process.exit(1)
