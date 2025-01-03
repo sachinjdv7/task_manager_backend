99e4dceb4902ba3549972e4cdf2178dfe003471e
sachinjadhav <sachinjdv7@gmail.com>
2025-01-03 16:27:39 +0530

    test lint-staged 2

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,13 @@
 function dispplayName(name: string) {
     console.log(`Welcome ${name}`)
+
+    const user = {
+        name: 'sachin',
+    }
+
+    const fname = user.name
+
+    return name + fname
 }
 
 dispplayName('sachin')
